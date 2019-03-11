0f41c50676048d936c684d8a888110cffcef929d
NatashaZJaved <nj329@bath.ac.uk>
2019-03-11 21:23:54 +0000

    Keypoints and FilterWithTaylor

---
+++ DerivGaussianBlurMatrix.m
@@ -0,0 +1,16 @@
+function G = DerivGaussianBlurMatrix(N,sigma,Coord)
+% If Coord == 0 then deriv w.r.t x
+% If Coord == 1 then deriv w.r.t y
+    G = zeros(2*N+1);
+    for x = -N:N
+        for y = -N:N
+            if (Coord == 0)
+                d = (x/sigma^2);
+            else
+                d = (y/sigma^2);
+            end
+            G(x+N+1,y+N+1) = d*(1/(2*pi*sigma^2))*exp(-0.5*(x^2+y^2)/sigma^2);
+        end
+    end
+end
+
---
+++ Keypoints.m
@@ -0,0 +1,66 @@
+function ThemPoints = Keypoints(Lowes)
+% Takes Lowes Pyramid as input and finds Keypoints
+% Lowes stored as
+% Lowes{Blurs, i} where (Blurs+1)_th sigma, i is subsampling level
+% Lowes{1,4} is Lowes image at scale sigma_0 and 8x smaller
+ThemPoints = cell(size(Lowes,1),size(Lowes,2));
+for Blurs = 2:size(Lowes,1)-1
+    for i = 1:size(Lowes,2)
+        for Patch_hor = 1:ceil(size(Lowes{1,i},1)./3)
+            for Patch_vert = 1:ceil(size(Lowes{1,i},2)./3)
+                for col=1:3
+                    Patch_end_hor = Patch_hor*3;
+                    Patch_end_vert = Patch_vert*3;
+                    if (Patch_hor*3>size(Lowes{1,i},1))
+                        Patch_end_hor = size(Lowes{1,i},1);
+                    end
+                    if (Patch_vert*3>size(Lowes{1,i},2))
+                        Patch_end_vert = size(Lowes{1,i},2);
+                    end
+                
+                    max_above = max(max(Lowes{Blurs+1,i}...
+                        (Patch_hor*3-2:Patch_end_hor,Patch_vert*3-2:Patch_end_vert,col)));
+                    max_curr = max(max(Lowes{Blurs,i}...
+                        (Patch_hor*3-2:Patch_end_hor,Patch_vert*3-2:Patch_end_vert,col)));
+                    max_below = max(max(Lowes{Blurs-1,i}...
+                        (Patch_hor*3-2:Patch_end_hor,Patch_vert*3-2:Patch_end_vert,col)));
+                    
+                    % Maybe threshold?
+                    [~, index] = max([max_above max_curr max_below]);
+                    
+                    if (index == 1)
+                        [x, y] = find(Lowes{Blurs+1,i}...
+                        (Patch_hor*3-2:Patch_end_hor,Patch_vert*3-2:Patch_end_vert,col) ...
+                        == max_above);
+                        x = x(1); y = y(1);
+                        x = Patch_hor*3 - 3 + x;
+                        y = Patch_vert*3 - 3 + y;
+                        ThemPoints{Blurs+1,i} = [ThemPoints{Blurs+1,i}; x;y];
+                    elseif (index == 2)
+                        [x, y] = find(Lowes{Blurs,i}...
+                        (Patch_hor*3-2:Patch_end_hor,Patch_vert*3-2:Patch_end_vert,col) ...
+                        == max_curr);
+                        x = x(1); y = y(1);
+                        x = Patch_hor*3 - 3 + x;
+                        y = Patch_vert*3 - 3 + y;
+                        ThemPoints{Blurs,i} = [ThemPoints{Blurs,i}; x;y];
+                    else
+                        [x,y] = find(Lowes{Blurs-1,i}...
+                        (Patch_hor*3-2:Patch_end_hor,Patch_vert*3-2:Patch_end_vert,col) ...
+                        == max_below);
+                        x = x(1); y = y(1);
+                        x = Patch_hor*3 - 3 + x;
+                        y = Patch_vert*3 - 3 + y;
+                        ThemPoints{Blurs-1,i} = [ThemPoints{Blurs-1,i}; x;y];
+                    end
+
+                end
+            end
+        end
+    end
+end
+
+
+
+end
+
